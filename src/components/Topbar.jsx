b3e7606e8894d1b58e1d6a7ba834ededd4e85791
dnspavankumar <dnspavankumar722@gmail.com>
2026-03-29 03:06:52 +0530

    removed unnecessary clickables on header

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -11,16 +11,6 @@ export default function Topbar() {
           <span className="topbar-notification-dot" />
         </Link>
         <ThemeToggle />
-        <button className="topbar-icon-btn" id="settings-btn">
-          <Settings size={18} />
-        </button>
-        <div className="topbar-user">
-          <div className="topbar-avatar">AC</div>
-          <div className="topbar-user-info">
-            <span className="topbar-user-name">Admin Console</span>
-            <span className="topbar-user-role">Operations Lead</span>
-          </div>
-        </div>
       </div>
     </header>
   )
